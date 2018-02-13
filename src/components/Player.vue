e545b8b6505946a6191d0a4d8a36c7d5bd71eed6
Ervin <bbuchala93@gmail.com>
2018-02-13 08:07:50 +0100

    First project commit.

---
+++ src/components/Player.vue
@@ -0,0 +1,29 @@
+<template>
+<h1>Here be player</h1>
+</template>
+
+<script>
+import {Howl, Howler} from 'howler'
+
+export default {
+  name: 'Player',
+  methods: {
+    initializePlayer: function () {
+      const sound = new Howl({
+        src: ['http://radio.vgmradio.com:8040/stream'],
+        ext: ['mp3'],
+        autoplay: true,
+        html5: true
+      })
+      sound.play()
+
+      Howler.volume(0.5)
+    }
+  },
+  beforeMount () {
+    this.initializePlayer()
+  }
+}
+</script>
+
+<style></style>
