71d3c6a7a187b371c911d9bc5ca4d5963f70ed76
Ervin <bbuchala93@gmail.com>
2018-02-15 09:06:55 +0100

    Impoved timers for metadata loading.

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import VueTimers from 'vue-timers'
 import MusicBar from '@/components/MusicBar'
 
 Vue.use(Router)
+Vue.use(VueTimers)
 
 export default new Router({
   routes: [
